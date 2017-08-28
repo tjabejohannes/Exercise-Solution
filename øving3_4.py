9f26691255d519b741eb91146bcafd36ea6fcbb9
tjabejohannes <tjabe_johannes@outlook.com>
2017-08-28 18:11:08 +0200

    Adding some exercises

--- øving3_4.py
+++ øving3_4.py
@@ -5,15 +5,18 @@ from time import sleep
 import sys
 
 vertices= ((-1.0,1.0,1.0),(-1.0,-1.0,1.0),(-1.0,-1.0,-1.0),(-1.0,1.0,-1.0),(1.0,-1.0,-1.0),(1.0,-1.0,1.0),(1.0,1.0,-1.0),(1.0,1.0,1.0))
-dt = 0.
+auto_rotate = False
 def init():
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
     glutInitWindowSize(1000,1000)
     glutCreateWindow("test")
-    glTranslate(-2.5,2.,-15.) #Centering the entire sett of objects
+    glTranslate(0.,0.,-10.) #Centering the entire sett of objects
+    glClearColor(1.,1.,1.,1.)   #White background
+    glColor3f(1.,0.,0.)         #Draw in red
     glutDisplayFunc(display)
-    #glutIdleFunc(myidel)
+    glutIdleFunc(myidel)
+    glutMouseFunc(mymouse)
     glutKeyboardFunc(mykey)
     glMatrixMode(GL_PROJECTION)
     gluPerspective(45.0,1.0/1.0,0.1,100)
@@ -26,9 +29,6 @@ def init():
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT)
-    glClearColor(1.,1.,1.,1.)   #White background
-    glColor3f(1.,0.,0.)         #Draw in red
-    #glRotatef(1, 3, 1, 1)
     glutWireCube(2.)
     glPushMatrix()
     glPopMatrix()
@@ -36,25 +36,37 @@ def display():
     return
 
 def myidel():
-    sleep(0.1)
+    sleep(0.01)
+    #print(auto_rotate)
+    if(auto_rotate):
+        glRotatef(1, 3, 1, 1)
     glutPostRedisplay()
 
 def mykey(key,x,y):
-    sleep(0.1)
+    #sleep(0.1)
     print(''.join(["key:",str(key)," x:",str(x)," y:",str(y)]))
-    if key=='w':
+    if key == 'w':
         glRotatef(10, 1, 0, 0)
-        print ("w pressed")
     elif key == 's':
         glRotatef(-10, 1, 0, 0)
-        print ("s pressed")
     elif key == 'a':
         glRotatef(10, 0, 1, 0)
-        print ("a pressed")
     elif key == 'd':
         glRotatef(10, 0, 0, 1)
-        print ("d pressed")
+    elif key == 'r':
+        #not working
+        auto_rotate = True
+    elif key == 'q':
+        print ("Escape pressed..")
+        exit(0)
     glutPostRedisplay()
+
+def mymouse(button, state, x, y):
+    print(''.join(["button:",str(button)," x:",str(x)," y:",str(y)]))
+    if button == 0:
+        glRotatef(10, 1, 0, 0)
+    elif button == 2:
+        glRotatef(-10, 1, 0, 0)
 
 def newObject(openglnotation):
     glBegin(openglnotation)
